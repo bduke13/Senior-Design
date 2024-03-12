6530c52ff0bbd261d8cde8d1096cad164ad31520
Andrew Gerstenslager <andrewgerstenslager@gmail.com>
2024-03-12 19:21:02 +0000

    updated documentation on sh file

--- src/start_container.sh
+++ src/start_container.sh
@@ -1,3 +1,6 @@
 #documentation found at https://github.com/iRobotEducation/create3-docker/tree/main
+#runs an example ros2 humble docker container. See the .devconatiner/Dockerfile for what
+#   container we are running and .devcontainer/devcontainer.json to see the launch arguments
+
 docker run -it --net=host --device=/dev/ttyUSB1 ros:humble-ros-core
 
